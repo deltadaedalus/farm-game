7e6ab3fe6bf8cea5576ce28e5bdc41008386f4f5
Chris Delta <Chris Delta>
2016-05-15 13:52:35 -0600

    Created Tileset

---
+++ Assets/TileSet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileSet : MonoBehaviour {
+    public GameObject flatPrefab;
+    public GameObject slopePrefab;
+    public GameObject outPrefab;
+    public GameObject inPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
