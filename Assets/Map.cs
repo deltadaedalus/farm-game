3420b6cd0e2a6db34c9e99a181bb28189caf7c09
Chris Delta <Chris Delta>
2016-05-15 18:31:51 -0600

    Map loading good

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class Map : MonoBehaviour {
     public TileSet[] tileSets;
+    public Texture2D mapTex;
+    public Texture2D heightTex;
 
     Tile[,] tileMap;
-
+    int width;
+    int height;
 
 	// Use this for initialization
 	void Start () {
-	    
+	    width = mapTex.width;
+        height = mapTex.height;
+        Debug.Log(width);
+        Debug.Log(height);
+        tileMap = new Tile[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                tileMap[x, y] = new Tile(new intVec2(x, y), (int)(heightTex.GetPixel(x, y).r * 32), tileSets[0]);
+                tileMap[x, y].setPiece(0, 0);
+            }
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,16 +41,16 @@ public class Tile
     intVec2 position;
     int height;
 
-    Tile(intVec2 position, int height, TileSet tileSet)
+    public Tile(intVec2 position, int height, TileSet tileSet)
     {
         this.height = height;
         this.position = position;
         this.tileSet = tileSet;
     }
 
-    void setPiece(int down, int rotation)
+    public void setPiece(int down, int rotation)
     {
-
+        piece = (GameObject)Object.Instantiate(tileSet.flatPrefab, new Vector3((float)position.x, height * 0.5f, (float)position.y), Quaternion.identity);
     }
 }
 
