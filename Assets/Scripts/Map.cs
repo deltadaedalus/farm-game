75bae6bf44b4b1831ca21daa26069bd03adb680d
Chris Delta <Chris Delta>
2016-05-18 13:06:24 -0600

    Map mostly done

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -19,19 +19,37 @@ public class Map : MonoBehaviour {
         tileMap = new Tile[width, height];
 
         for (int x = 0; x < width; x++)
-        {
+            for (int y = 0; y < height; y++)
+                tileMap[x, y] = new Tile(new intVec2(x, y), (int)(heightTex.GetPixel(x, y).r * 32), tileSets[0]);
+
+        for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
             {
-                tileMap[x, y] = new Tile(new intVec2(x, y), (int)(heightTex.GetPixel(x, y).r * 32), tileSets[0]);
-                tileMap[x, y].setPiece(0, 0);
+                int pp = System.Math.Max(System.Math.Max(getTile(x + 1, y + 1).height, getTile(x, y + 1).height), getTile(x + 1, y).height);
+                int pn = System.Math.Max(System.Math.Max(getTile(x + 1, y - 1).height, getTile(x, y - 1).height), getTile(x + 1, y).height);
+                int np = System.Math.Max(System.Math.Max(getTile(x - 1, y + 1).height, getTile(x, y + 1).height), getTile(x - 1, y).height);
+                int nn = System.Math.Max(System.Math.Max(getTile(x - 1, y - 1).height, getTile(x, y - 1).height), getTile(x - 1, y).height);
+                tileMap[x, y].setPiece(pp, pn, np, nn);
             }
-        }
+                
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    
 	}
+
+    Tile getTile(int x, int y) {
+        x = Util.mod(x, width);
+        y = Util.mod(y, height);
+
+        return tileMap[x, y];
+    }
+
+    float getHeight()
+    {
+        return 0.0f;
+    }
 }
 
 public class Tile
@@ -39,7 +57,7 @@ public class Tile
     GameObject piece;
     TileSet tileSet;
     intVec2 position;
-    int height;
+    public int height;
 
     public Tile(intVec2 position, int height, TileSet tileSet)
     {
@@ -48,9 +66,36 @@ public class Tile
         this.tileSet = tileSet;
     }
 
-    public void setPiece(int down, int rotation)
+    public void setPiece(int pp, int pn, int np, int nn)
     {
-        piece = (GameObject)Object.Instantiate(tileSet.flatPrefab, new Vector3((float)position.x, height * 0.5f, (float)position.y), Quaternion.identity);
+        int pieceNum = 0;
+        Vector3 pos = new Vector3((float)position.x, height * 0.5f, (float)position.y);
+
+        if (pp - height == 1) pieceNum += 1;
+        if (pn - height == 1) pieceNum += 2;
+        if (np - height == 1) pieceNum += 4;
+        if (nn - height == 1) pieceNum += 8;
+
+        //This could potentially be prettier, but 16 is a small number so fuck it
+        switch(pieceNum) {
+            case 0: piece = (GameObject)Object.Instantiate(tileSet.flatPrefab, pos, tileSet.flatPrefab.transform.rotation); break;
+            case 1: piece = (GameObject)Object.Instantiate(tileSet.outPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(180, Vector3.forward)); break;
+            case 2: piece = (GameObject)Object.Instantiate(tileSet.outPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(270, Vector3.forward)); break;
+            case 3: piece = (GameObject)Object.Instantiate(tileSet.slopePrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(270, Vector3.forward)); break;
+            case 4: piece = (GameObject)Object.Instantiate(tileSet.outPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(90, Vector3.forward)); break;
+            case 5: piece = (GameObject)Object.Instantiate(tileSet.slopePrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(180, Vector3.forward)); break;
+
+            case 7: piece = (GameObject)Object.Instantiate(tileSet.inPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(180, Vector3.forward)); break;
+            case 8: piece = (GameObject)Object.Instantiate(tileSet.outPrefab, pos, tileSet.flatPrefab.transform.rotation); break;
+
+            case 10: piece = (GameObject)Object.Instantiate(tileSet.slopePrefab, pos, tileSet.flatPrefab.transform.rotation); break;
+            case 11: piece = (GameObject)Object.Instantiate(tileSet.inPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(270, Vector3.forward)); break;
+            case 12: piece = (GameObject)Object.Instantiate(tileSet.slopePrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(90, Vector3.forward)); break;
+            case 13: piece = (GameObject)Object.Instantiate(tileSet.inPrefab, pos, tileSet.flatPrefab.transform.rotation * Quaternion.AngleAxis(90, Vector3.forward)); break;
+            case 14: piece = (GameObject)Object.Instantiate(tileSet.inPrefab, pos, tileSet.flatPrefab.transform.rotation); break;
+
+            default: piece = (GameObject)Object.Instantiate(tileSet.flatPrefab, pos, tileSet.flatPrefab.transform.rotation); break;
+        }
     }
 }
 
---
+++ Assets/Scripts/SmartenSharp.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util
+{
+    public static int mod(int x, int m)
+    {
+        return (x % m + m) % m;
+    }
+}
